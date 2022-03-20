5abc19e75614ea2dee55f491d1bb5e37ac0db139
Harrison <56010813+hthegoat@users.noreply.github.com>
2022-03-20 15:44:46 -0400

    Launch Site

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,11 @@
 <template>
-<div> 
-  <Hero />
-  
+<div class="snap-y"> 
+  <Hero/>
+  <Hero2 class="snap-center"/>
+  <Hero3 />
+  <Testimonial />
+  <ClientLogos />
+    <MainContact />
 </div>
 </template>
 
